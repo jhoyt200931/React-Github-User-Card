eabf0325c5bd30be12dd1631a58eb3a1beb23b64
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-09-04 23:24:05 -0400

    built out components and styled

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,27 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserCard from "./UserCard";
+import styled from "styled-components";
+
+
+const Title = styled.h1`
+  font-size: 3em;
+  text-decoration: underline;
+  margin-left: 35%;
+`;
+
+const Div = styled.div`
+  background-image: url("https://www.whiteheadassoc.com/wp-content/uploads/2018/09/iStock-998993038.jpg");
+  height: 750px;
+`;
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      userInfo: {},
+      followers: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/jhoyt200931")
+      .then(res => res.json())
+        .then((json) => {
+          this.setState({
+            userInfo: json
+          })
+          console.log("User Info: ", this.state.userInfo)
+        })
+          .catch(err => {
+            console.log(err);
+          });
+    fetch("https://api.github.com/users/jhoyt200931/followers")
+      .then(res => res.json())
+          .then((json) => {
+            this.setState({
+              followers: json
+            })
+            console.log("Followers: ",this.state.followers)
+          })
+            .catch(err => {
+              console.log(err);
+            });
+  }
+
   render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <Title>Github Usercards</Title>
+      <UserCard userInfo={this.state.userInfo} />
+      {this.state.followers.map(follower => {
+        return <UserCard key={follower.id} userInfo={follower}/>
+      })}
+    </Div>
   );
   }
 }
--- github-usercard/src/UserCard.js
+++ github-usercard/src/UserCard.js
@@ -1,10 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 
-const UserCard = () => {
+
+const Div = styled.div`
+    width: 50%;
+    border: 1px solid black;
+    margin-left: 25%;
+    margin-bottom: 20px; 
+    display: flex;
+    background-color: gray;
+    
+`;
+
+const Info = styled.div`
+    margin-left: 10%;
+`;
+
+const Img = styled.img`
+    width: 30%;
+    height: 200px;
+`;
+
+const Username = styled.p`
+    font-size: 1.1em;
+    border-bottom: 1px solid black;
+    margin-bottom: 5px;
+`;
+
+const Location = styled.p`
+    font-style: italic;
+`;
+
+
+const UserCard = (props) => {
+
+    console.log(props);
 
     return(
-        <div>
-            
-        </div>
+        <Div>
+            <Img src={props.userInfo.avatar_url} />
+            <Info>
+                <Username>{props.userInfo.login}</Username>
+                <Location>{props.userInfo.location}</Location>
+                <a href={props.userInfo.html_url}>{props.userInfo.login}'s profile</a>
+            </Info>
+        </Div>
     )
 }
+
+export default UserCard;
